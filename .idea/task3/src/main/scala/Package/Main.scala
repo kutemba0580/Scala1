b3430e46ab6aae2c920a61385573f0ebc1001e7f
MPISHI <mchangakutemba.com>
2019-10-15 04:47:55 +0500

    homework1

---
+++ .idea/task3/src/main/scala/Package/Main.scala
@@ -0,0 +1,17 @@
+package Package
+
+object Main extends App {
+  val name : String = "Mchanga Kutemba";
+  val words : String = " Scala! This is ";
+
+
+
+  println("Hello Scala! This is"+name);
+  println("Bonjour"+name+words);
+  println("Bonjour "+words+name);
+  println("Marhaba "+words+name);
+  println("Hola "+words+name)
+  println("Hola "+words+name.reverse)
+  println("Bonjour "+words+name.reverse)
+  println("Marhaba "+words+name.reverse)
+}
